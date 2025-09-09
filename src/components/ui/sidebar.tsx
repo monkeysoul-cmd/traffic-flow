894f60d364037023308302b1908037adf12f36a1
Ayush Rawal <ayushrawal2003@gmail.com>
2025-09-09 15:48:08 +0000

    change the tile color to green if it is opened

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -135,7 +135,7 @@ const SidebarProvider = React.forwardRef<
           <div
             style={
               {
-                "--sidebar-width": SIDEBAR_WIDTH,
+                "--sidebar-width": "12rem",
                 "--sidebar-width-icon": SIDEBAR_WIDTH_ICON,
                 ...style,
               } as React.CSSProperties
